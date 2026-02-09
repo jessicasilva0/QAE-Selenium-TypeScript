7a5246908436e593008901c7654a637529eef822
Jess <90346963+JessicaSilva0@users.noreply.github.com>
2026-02-10 00:34:32 +0100

    feat: add eslint .
    added eslint and refactor README.MD to ensure run specific tests using scripts .

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["dist/"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.mocha },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
