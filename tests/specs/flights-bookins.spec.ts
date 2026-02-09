b1c8527fec6b491c8771aa5fb3266fb961d02f79
Jess <90346963+JessicaSilva0@users.noreply.github.com>
2026-02-09 21:54:10 +0100

    chore: add new test cases specs flight-bookins and login

---
+++ tests/specs/flights-bookins.spec.ts
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+import {
+  loginPage,
+  dashboardPage,
+  flightBookingPage,
+} from "../fixtures/pages-fixtures";
+
+dotenv.config();
+
+const emailAddress = process.env.email;
+const password = process.env.password;
+
+describe("Flights Bookings", () => {
+  beforeEach(async () => {
+    await loginPage.goToLoginPage();
+    await loginPage.insertCredentials(emailAddress!, password!);
+    await loginPage.clickSignInButton();
+    await dashboardPage.goToFlightBooking();
+  });
+
+  it("TC-01: Should show alert when searching flights without departure and arrival", async () => {
+    await flightBookingPage.selectDepartureAndArrivalDates(
+      "",
+      "",
+      "20-02-2026",
+    );
+  });
+
+  it("TC-02: Should show alert when searching flights without departure airport", async () => {
+    await flightBookingPage.selectDepartureAndArrivalDates(
+      "",
+      "LIS",
+      "20-02-2026",
+    );
+  });
+
+  it("TC-03: Should show alert when searching flights without arrival airport", async () => {
+    await flightBookingPage.selectDepartureAndArrivalDates(
+      "OPO",
+      "",
+      "20-02-2026",
+    );
+  });
+
+  it("TC-04: Should allow entering a valid departure date", async () => {
+    await flightBookingPage.departureDateInput("15-02-2026");
+  });
+});
--- tests/specs/login.spec.ts
+++ tests/specs/login.spec.ts
@@ -1,20 +1,20 @@
 import dotenv from "dotenv";
-import {
-  loginPage,
-  dashboardPage,
-  flightBookingPage,
-} from "../fixtures/pages-fixtures";
+import { loginPage, dashboardPage } from "../fixtures/pages-fixtures";
+
 dotenv.config();
 
 const emailAddress = process.env.email;
 const password = process.env.password;
 
 describe("Login", () => {
-  it("TC-01: Should login page", async () => {
+  it("TC-01: Should open login page when navigating to base URL", async () => {
+    await loginPage.goToLoginPage();
+  });
+
+  it("TC-02: Should login successfully with valid credentials and display dashboard", async () => {
     await loginPage.goToLoginPage();
     await loginPage.insertCredentials(emailAddress!, password!);
     await loginPage.clickSignInButton();
-    await dashboardPage.goToFlightBooking();
-    await flightBookingPage.selectDepartureAndArrivalDates("", "", "");
+    await dashboardPage.validateRecentBookingScreen();
   });
 });
